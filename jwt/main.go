0c616b677865e1435e9e0a3811589956cbde5d3c
athunlal <athunlalp@gmail.com>
2023-07-02 19:33:34 +0530

    jwt edited

--- jwt/main.go
+++ jwt/main.go
@@ -39,34 +39,9 @@ func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, tokenString)
 }
 
-func protectedPage(w http.ResponseWriter, r *http.Request) {
-	// Extract the token from the Authorization header
-	tokenString := r.Header.Get("Authorization")[7:] // Remove the "Bearer " prefix
-
-	// Parse the token
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Make sure that the signing method is HMAC with the secret key
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return jwtKey, nil
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check if the token is valid
-	if token.Valid {
-		fmt.Fprintf(w, "Welcome to the protected page!")
-	} else {
-		fmt.Fprintf(w, "Invalid token!")
-	}
-}
-
 func main() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/login", login)
-	http.HandleFunc("/protected", protectedPage)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
