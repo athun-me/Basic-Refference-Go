9210fa7e4d8c2686566c67b61f2779b4270d2dbd
athunlal <athunlalp@gmail.com>
2023-11-11 21:31:00 +0530

    canceletion propagation context

---
+++ canceletionPropagationContext/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func webServer(w http.ResponseWriter, r *http.Request) {
+	timer := time.NewTimer(10 * time.Second)
+	select {
+	case <-r.Context().Done():
+		log.Println("Error when processing request:", r.Context().Err())
+		return
+	case <-timer.C:
+		log.Println("writing response...")
+		_, err := io.WriteString(w, "Hello context")
+		if err != nil {
+			log.Println("Error when writing response", err)
+		}
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", webServer)
+	log.Println("starting web server...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
