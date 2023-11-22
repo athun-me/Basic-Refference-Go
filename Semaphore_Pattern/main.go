67dfc4097a14baeb1fe672bc37f0e3a4c31a7c16
athunlal <athunlalp@gmail.com>
2023-11-22 12:05:10 +0530

    Semaphore pattern

--- Semaphore_Pattern/main.go
+++ Semaphore_Pattern/main.go
@@ -2,32 +2,31 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"time"
 )
 
 func main() {
+	wg := &sync.WaitGroup{}
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-	wg := sync.WaitGroup{}
-
 	queue := make(chan bool, 3)
-
-	for _, val := range arr {
+	for _, id := range arr {
 		wg.Add(1)
-		go func(val int) {
+		go func(id int) {
 			defer wg.Done()
 
 			queue <- true
-			defer func() {
-				<-queue
-			}()
-			req(val, &wg)
-		}(val)
+			defer func() { <-queue }()
+			makeRequest(id)
+		}(id)
 	}
+
+	fmt.Println("number of goroutine", runtime.NumGoroutine())
 	wg.Wait()
 }
 
-func req(i int, wg *sync.WaitGroup) {
-	time.Sleep(1 * time.Second)
-	fmt.Println(i)
+func makeRequest(id int) {
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("id :", id)
 }
